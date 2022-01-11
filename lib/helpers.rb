2e5aeb069cb9756f3b99ba60a799eb9de5925e50
protoBASILISK <pionlaversti@gmail.com>
2022-01-11 03:17:18 +0000

    Translated Entity + most of the rest of the existing base to JSON.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,7 +2,7 @@
 
 # Iterates over collections to see if there's any substantive data in them.
 # Checks nested arrays/hashes to see if there's any in there.
-def is_empty( data_to_test)
+def is_empty( data_to_test )
   return true if data_to_test.nil?
   return true if data_to_test.empty?
 
@@ -12,15 +12,18 @@ def is_empty( data_to_test)
       return false if not is_empty( inner_val )
     end
     return true
+
   elsif data_to_test.is_a? Hash
+
     data_to_test.each do |val|
         _values = val.select { |k,v| v.present? }
         return false if _values.any?
     end
-    return true
+
   end
 
-  return false
+  return false if data_to_test.present?
+  return true
 end
 
 def load_json_data( abs_path )
@@ -36,6 +39,8 @@ def preprocess_json_data( json_data, variable_hash )
     json_data.collect!{ |var| preprocess_json_data( var, variable_hash ) }
   elsif json_data.is_a? Hash
     json_data.each { |key, value| json_data[key] = preprocess_json_data( value, variable_hash ) }
+  elsif json_data.is_a? TrueClass or json_data.is_a? FalseClass
+
   elsif json_data.present?
     json_data = ERB.new( json_data ).result_with_hash( variable_hash )
   end
